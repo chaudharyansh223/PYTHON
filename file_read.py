dd6e3e295ca516ca88cad4c01e4cbec24deb66fb
ANSH CHAUDHARY <chaudharyansh223@gmail.com>
2026-09-21 07:43:09 +0530

    I worked with error and file handling

--- file_read.py
+++ file_read.py
@@ -1,10 +1,23 @@
-modified_file_content = []
-def read():
-    with open("app.log", "r") as file:
-        for line in file:
-            if line.startswith("ERROR:"):
-                new_line = line.replace("ERROR: ", "")
-                modified_line = new_line.strip()
-                modified_file_content.append(modified_line)
-    print(modified_file_content)
-read()
+file_read = {"status": "", "content": ""}
+file_read_error = {}
+def read_file(file_path):
+    try:
+        with open(file_path, "r") as file:
+            content = file.read()
+            if content:
+                    with open("content.txt", "w") as File:
+                        File.write(content)
+                    file_read["status"] = "succuess"
+                    file_read["content"] = "read in content.txt"
+                    return file_read
+    except FileNotFoundError:
+        file_read_error["status"] = "error"
+        file_read_error["message"] = "file not found"
+        return file_read_error
+    finally:
+        print("file read operation attempted")
+
+print(read_file("missing_file.txt"))
+
+
+                
